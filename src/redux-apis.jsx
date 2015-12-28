464aa3cb9afb48560d3df07ad6a6dd5d632b7483
Stijn de Witt <StijnDeWitt@hotmail.com>
2015-12-28 15:56:06 +0100

    0.1.0

---
+++ src/redux-apis.jsx
@@ -0,0 +1,88 @@
+﻿import { createAction } from 'redux-actions';
+
+export default class Api {
+	constructor(state) {
+		Object.defineProperty(this, 'state', {
+			get: function() {
+				if (this.parent) {
+					const name = childName(this.parent, this);
+					return this.parent.state[name];
+				}
+				if (this.store) {
+					return this.store.getState();
+				}
+				return state;
+			},
+			set: function(value) {
+				if (!this.parent && !this.store) {
+					state = value;
+				}
+			}
+		});
+	}
+
+	sub(name, api) {
+		this[name] = new api();
+		this[name].parent = this;
+	}
+
+	createAction(actionType, payload, ...args) {
+		const name = this.parent && childName(this.parent, this);
+		if (name) {
+			return this.parent.createAction(name + '/' + actionType, payload, ...args);
+		} else {
+			return createAction(actionType)(payload, ...args);
+		}
+	}
+
+	dispatch(action) {
+		if (this.parent) {
+			this.parent.dispatch(action);
+		} else if (this.store) {
+			this.store.dispatch(action);
+		} else {
+			// root, but not connected to store, exec directly
+			this.state = this.execute(action);
+		}
+	}
+
+	execute(action) {
+		const idx = action.type.indexOf('/');
+		const sub = idx !== -1 && action.type.substring(0, idx);
+		const subs = Object.keys(this);
+		let result = this.handle(action);
+		if (result === this.state) {result = undefined;}
+		for (let i=0,key; key=subs[i]; i++) {
+			if (key != 'parent' && this[key] instanceof Api) {
+				let act = key !== sub ? action : { ...action, type: action.type.substring(idx + 1) };
+				let subState = this[key].execute(act);
+				if (this.state[key] !== subState) {
+					result ? result[key] = subState : result = { ...this.state, [key]: subState };
+				}
+			}
+		}
+		return result || this.state;
+	}
+
+	handle(action) {
+		return this.state || {};
+	}
+}
+
+export function createReducer(instance) {
+	return (state, action) => {
+		return instance.execute(action);
+	}
+}
+
+export function bind(instance, store) {
+	instance.store = store;
+}
+
+function childName(parent, child) {
+	const siblings = Object.keys(parent);
+	for (let i=0,name; name=siblings[i]; i++) {
+		if (parent[name] === child) {return name;}
+	}
+}
+
---
+++ src/redux-apis.spec.jsx
@@ -0,0 +1,115 @@
+﻿import { expect } from 'chai';
+import { createStore } from 'redux';
+import Api, { createReducer, bind } from './redux-apis';
+
+describe('redux-apis', () => {
+	it('provides helpers to build redux-aware API\'s', () => {});
+
+	describe('Api (default export)', () => {
+		it('is a class that serves as a base class for redux-aware API\'s', () => {
+			expect(Api).to.not.equal(null);
+			expect(Api).to.be.a('function');
+			class MyApi extends Api {};
+			expect(MyApi.prototype).to.be.an.instanceOf(Api);
+		});
+
+		describe('constructor', () => {
+			it('can be overidden to add sub Apis', () => {
+				class SubApi extends Api {};
+				class MyApi extends Api {
+					constructor() {
+						super();
+						this.sub('nested', SubApi);
+						expect(this.nested).to.be.an.instanceOf(SubApi);
+
+					}
+				};
+				const myApi = new MyApi();
+			});
+		});
+
+		describe('sub', () => {
+			it('is a function available on instances of Api', () => {
+				class MyApi extends Api {};
+				const myApi = new MyApi();
+				expect(myApi).to.have.a.property('sub');
+				expect(myApi.sub).to.be.a('function');
+			});
+
+			it('accepts a name and Api class and adds an instance of that class', () => {
+				class MyApi extends Api {};
+				const myApi = new MyApi();
+				myApi.sub('nested', MyApi);
+				expect(myApi).to.have.a.property('nested');
+				expect(myApi.nested).to.be.an.instanceOf(MyApi);
+			});
+
+			it('sets a property parent on the created instance', () => {
+				class MyApi extends Api {};
+				const myApi = new MyApi();
+				myApi.sub('nested', MyApi);
+				expect(myApi.nested).to.have.a.property('parent');
+				expect(myApi.nested.parent).to.equal(myApi);
+			});
+		});
+
+		describe('handle', () => {
+			it('is a function available on instances of Api', () => {
+				class MyApi extends Api {};
+				const myApi = new MyApi();
+				expect(myApi.handle).to.be.a('function');
+			});
+
+			it('is called for each action that is dispatched to this api tree', () => {
+				let called = false;
+				class MyApi extends Api {
+					handle(action) {
+						expect(action).to.not.equal(undefined);
+						expect(action).to.have.a.property('type');
+						expect(action.type).to.equal('TEST');
+						called = true;
+						super.handle(action);
+					}
+				};
+				const myApi = new MyApi();
+				expect(called).to.equal(false);
+				myApi.dispatch(myApi.createAction('TEST'));
+				expect(called).to.equal(true);
+			});
+
+			it('should be overidden to handle actions', () => {
+			});
+		});
+	});
+
+	describe('createReducer', () => {
+		it('is a function', () => {
+			expect(createReducer).to.be.a('function');
+		});
+
+		it('accepts an instance of Api and returns a reducer function', () => {
+			class MyApi extends Api {};
+			const instance = new MyApi();
+			const reducer = createReducer(instance);
+			expect(reducer).to.be.a('function');
+		});
+	});
+
+	describe('bind', () => {
+		it('is a function', () => {
+			expect(bind).to.be.a('function');
+		});
+
+		it('accepts an instance of Api and a store and binds the store to the instance', () => {
+			class MyApi extends Api {};
+			const instance = new MyApi();
+			const reducer = createReducer(instance);
+			const store = createStore(reducer);
+			bind(instance, store);
+			expect(instance).to.have.a.property('store');
+			expect(instance.store).to.have.a.property('getState');
+			expect(instance.store.getState).to.be.a('function');
+			expect(instance.store.getState()).to.equal(instance.state);
+		});
+	});
+});
